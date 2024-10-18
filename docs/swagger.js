87cced1a180ede0cbb406de9771dbfdd72449538
Meriem Soubih <soubih.meriem@gmail.com>
2024-10-18 21:40:31 +0100

    docs: init swagger docs

---
+++ docs/swagger.js
@@ -0,0 +1,24 @@
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "MedCab API",
+      version: "1.0.0",
+      description: "API documentation for the MedCab backend",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+        description: "Local server",
+      },
+    ],
+  },
+  apis: ["./routes/*.routes.js"],
+};
+
+const specs = swaggerJsDoc(options);
+
+module.exports = { swaggerUi, specs };
