2aecf35ea4392ad69cf8546d4e651a9d7f1b1aed
Meriem Soubih <soubih.meriem@gmail.com>
2024-11-30 08:54:56 +0100

    -

---
+++ models/Doctor.js
@@ -0,0 +1,43 @@
+const { sequelize } = require('../config/postgres');
+
+const Doctor = sequelize.define('Doctor', {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    phone: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    address: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    specialty: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+    },
+    deletedAt: {
+        type: DataTypes.DATE
+    }
+});
+
+// Doctor.hasMany(Patient);
+// Patient.belongsTo(Doctor);
