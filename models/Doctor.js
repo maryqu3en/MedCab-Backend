6a6b30fa8636927460e15bd86e3df04cb6ac058e
Meriem Soubih <soubih.meriem@gmail.com>
2025-03-22 15:35:54 +0100

    -

--- models/Doctor.js
+++ models/Doctor.js
@@ -6,27 +6,29 @@ module.exports = (sequelize) => {
             type: DataTypes.UUID,
             defaultValue: DataTypes.UUIDV4,
             primaryKey: true,
+            references: {
+                model: 'Users',
+                key: 'id',
+            },
         },
-        Name: { type: DataTypes.STRING, allowNull: false },
-        Email: { type: DataTypes.STRING, allowNull: false, unique: true },
-        Password: { type: DataTypes.STRING, allowNull: false },
         Phone: { type: DataTypes.STRING },
         Address: { type: DataTypes.STRING },
         Specialty: { type: DataTypes.STRING },
         CreatedBy: { type: DataTypes.UUID },
     },
-    {
-        timestamps: true,
-        paranoid: true,
-        indexes: [
-            {
-                fields: ['Email'],
-                unique: true,
-            }
-        ],
-    });
+        {
+            timestamps: true,
+            paranoid: true,
+            indexes: [
+                {
+                    fields: ['Email'],
+                    unique: true,
+                }
+            ],
+        });
 
     Doctor.associate = (models) => {
+        Doctor.belongsTo(models.User, { foreignKey: 'ID', as: 'User' });
         Doctor.belongsTo(models.Admin, { foreignKey: 'CreatedBy', as: 'Creator' });
         Doctor.hasMany(models.Appointment);
     };
