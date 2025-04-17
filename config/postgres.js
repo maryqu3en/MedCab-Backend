f51452ed741df93ddc0f17bcee9ec51319695ffc
Meriem Soubih <soubih.meriem@gmail.com>
2025-04-17 12:07:39 +0100

    add requirement

--- config/postgres.js
+++ config/postgres.js
@@ -1,4 +1,6 @@
 const { Sequelize, DataTypes } = require('sequelize');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const sequelize = new Sequelize(process.env.PG_DATABASE, process.env.PG_USER, process.env.PG_PASSWORD, {
   host: process.env.PG_HOST,
